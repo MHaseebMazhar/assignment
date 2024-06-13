f22d029df5b77cdf1574e5c655c317f527dc4a81
Muhammad Haseeb <126347233+MHaseebMazhar@users.noreply.github.com>
2024-06-13 18:13:05 +0500

    for loop array
    Implement number array and Print value with for loop

--- assignment/assignment/main.cpp
+++ assignment/assignment/main.cpp
@@ -1,12 +1,18 @@
-//
-//  main.cpp
-//  assignment
-//
-//  Created by Shaista Awan on 13/06/2024.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    gfhgfh
+#include<iostream>
+using namespace std;
+int main(){
+    int num[] ={23, 22, 44, 66, 88, 99, 12, 43, 32, 53};
+//    cout <<num[0] <<endl;
+//    cout <<num[1] <<endl;
+//    cout <<num[2] <<endl;
+//    cout <<num[3] <<endl;
+//    cout <<num[4] <<endl;
+//    cout <<num[5] <<endl;
+//    cout <<num[6] <<endl;
+//    cout <<num[7] <<endl;
+//    cout <<num[8] <<endl;
+//    cout <<num[9] <<endl;
+    for (int i =0; i < 10; i ++) {
+        cout <<"The num is "<<i<<" = "<<num[i]<<endl;
+    }
 }
