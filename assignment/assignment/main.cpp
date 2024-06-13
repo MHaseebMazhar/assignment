0341e881bd1591be32332f4b701163786658c059
Muhammad Haseeb <126347233+MHaseebMazhar@users.noreply.github.com>
2024-06-13 17:39:13 +0500

    initial commit
    Starting Project

---
+++ assignment/assignment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment
+//
+//  Created by Shaista Awan on 13/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
